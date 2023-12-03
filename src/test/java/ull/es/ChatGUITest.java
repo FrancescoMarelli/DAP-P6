8e631d045bdb240f13f2761dfa28182f7a31af18
FrancescoMarelli <alu0101161730@ull.edu.es>
2023-12-03 15:57:18 +0000

    ChatGUITest

--- src/test/java/ull/es/ChatGUITest.java
+++ src/test/java/ull/es/ChatGUITest.java
@@ -1,6 +1,11 @@
 import org.junit.Before;
 import org.junit.Test;
+
+import javax.swing.*;
+
 import static org.junit.Assert.*;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
 
 public class ChatGUITest {
@@ -11,6 +16,34 @@ public class ChatGUITest {
     public void setUp() {
         chatGUI = new ChatGUI();
     }
+    @Test
+    public void testChatGUIInitialization() {
+        ChatGUI chatGUI = new ChatGUI();
+
+        // Verifica que el título de la ventana sea "ChatRoom"
+        assertEquals("ChatRoom", chatGUI.getTitle());
+
+        // Verifica que el cierre de la ventana esté configurado correctamente
+        assertEquals(JFrame.EXIT_ON_CLOSE, chatGUI.getDefaultCloseOperation());
+
+        // Verifica que el layout de la ventana sea BorderLayout
+        assertTrue(chatGUI.getLayout() instanceof BorderLayout);
+
+        // Verifica que la ventana no sea redimensionable
+        assertFalse(chatGUI.isResizable());
+
+        // Verifica que la ventana sea visible
+        assertTrue(chatGUI.isVisible());
+
+        // Verifica que el tamaño de la ventana sea (300, 150)
+        assertEquals(new Dimension(300, 150), chatGUI.getSize());
+
+        // Verifica que la ubicación de la ventana sea centrada
+        assertNotNull(chatGUI.getLocation());
+
+        // Podrías agregar más verificaciones según sea necesario para la inicialización
+    }
+
 
     @Test
     public void testStartPrivateChat() {
